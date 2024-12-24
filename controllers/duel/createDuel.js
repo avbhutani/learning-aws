16c7c3050af949ab85ae766be546462b1eece4c6
avbhutani <avbhutani3@gmail.com>
2024-12-24 13:47:32 +0530

    Added Duel Creation

---
+++ controllers/duel/createDuel.js
@@ -0,0 +1,72 @@
+const AWS = require('aws-sdk')
+const { v4: uuidv4 } = require('uuid'); 
+async function duelCreation(body,expiryTime) {
+    const creatorID = body.creatorID
+    const duel = {
+        creatorID,
+        prizeAmount: body.prizeAmount || 0,
+        submissionLimit: body.submissionLimit || 25,
+        currentSubmissions: 0,
+        nicheCategories: body.nicheCategories && body.nicheCategories.length > 0 ? body.nicheCategories : undefined,
+        videoDuration: body.videoDuration || 30,
+        contentType: body.contentType || 'organic',
+        contentScript: body.contentScript && body.contentScript.length > 0 ? body.contentScript : undefined,
+        contentBrief: body.contentBrief && body.contentBrief.length > 0 ?  body.contentBrief : undefined,
+        contentExamples: body.contentExamples && body.contentExamples.length > 0 ? body.contentExamples : undefined, // Images
+        usefulAssets: body.usefulAssets && body.usefulAssets.length > 0 ? body.usefulAssets : undefined, // Images
+        currentStatus: 'active',  // can be only ['active','inactive']
+        endTime: expiryTime, // 
+        winners: [],
+        submissions: []
+      };
+      
+      // Remove undefined fields to avoid storing empty attributes in DynamoDB
+    for (const key in duel) {
+        if (duel[key] === undefined) {
+            delete duel[key];
+        }
+    }
+    return duel
+}
+
+async function createDuel(req,res) {
+    const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+    const expiryTime = currentTime + 30 * 24 * 60 * 60; // Add 30 days
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+    // fetch the user id from the JWT token and then store it in the variable
+    /* // do it here.
+     The creator ID basically says the creator that is creating the particular duel.
+    //     */ 
+    //    const creatorID = req.body.creatorID // fetch it from JWT
+   
+   
+
+    try {
+        const duel = await duelCreation(req.body,expiryTime)
+        console.log(duel)
+        const params = {
+            TableName:'duels',
+            Item: {
+                'id':uuidv4(),
+                ...duel
+            }
+        }
+        await dynamoDB.put(params).promise()
+        res.status(200).send(
+            {
+                success:'true',
+                message:'Created Duel Successfully!'
+            }
+        )
+    } catch (error) {
+        res.status(500).send(
+            {
+                success:'false',
+                message:'Failed to Create Duel!',
+                error
+            }
+        )
+    }
+}
+
+module.exports = createDuel
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const createNewCreator = require('./controllers/creators/createNewCreator')
 const creatorQueryByEmail = require('./controllers/creators/creatorQueryByEmail')
 const existingCreator = require('./middlewares/existingCreator')
 const brandQueryByEmail = require('./controllers/brands/brandQueryByEmail')
+const createDuel = require('./controllers/duel/createDuel')
 
 require('dotenv').config()
 require('./config')
@@ -21,15 +22,22 @@ app.use(bodyParser.json())
 // ? question mark denotes 0 can also be the occurence number for that particular brandID,
 // that is the brandID can be empty!
 // create a new brand.
+
+// brands routes
 app.post('/brands',createNewBrand)
-
-
 app.get('/brands/:id?',checkForID,fetchBrandById)
 app.put('/brands/:id?',checkForID,updateBrand)
 app.delete('/brands/:id?',checkForID,deleteBrand)
 app.post('/brands/search',brandQueryByEmail)
+
+// creator routes
 app.post('/creators',checkEmail,existingCreator,createNewCreator)
 app.post('/creators/search',creatorQueryByEmail)
+
+
+// duel routes
+app.post('/duel',createDuel)
+
 app.listen(process.env.PORT || 4000, ()=> {
     console.log(`Server is listening at ${process.env.PORT || 4000}`)
 })
